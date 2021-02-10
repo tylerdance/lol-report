ab13af29c24ac349f35cd040794bcb1ed1be45b0
Tyler Dance <tylerdance@Tylers-iMac.fios-router.home>
2021-02-10 09:43:08 -0500

    displaying first summoner name

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
 
+import FetchData from './components/FetchData'
+
 function App() {
   return (
     <div className="App">
-      
+      <FetchData />
     </div>
   );
 }
--- src/components/FetchData.js
+++ src/components/FetchData.js
@@ -1,14 +1,25 @@
 import axios from 'axios'
+import { useState } from 'react';
 
-function FetchData() {
-    axios.get('')
+const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
+
+function FetchData(props) {
+    const [data, setData] = useState('')
+    // const name = props.summName.map((p, index) => {
+        
+    // })
+    axios.get(`${REACT_APP_SERVER_URL}/api/request/request`)
     .then(res => {
-        console.log(res);
+        console.log(res.data[0].summonerName);
+        setData(res.data[0].summonerName)
+        // console.log(res.data.entries[0].summonerName);
+        // setData(res.data.entries[0].summonerName)
+
     })
 
     return(
         <div>
-
+            {data}
         </div>
     )
 }
