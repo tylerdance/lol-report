9140c039f4e549a6117c7f07add1fac402edc1e3
Tyler Dance <tylerdance@Tylers-iMac.fios-router.home>
2021-02-17 16:29:40 -0500

    minor styling adjustments

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ import PatchNotes from './components/PatchNotes'
 function App() {
   return (
     <div className="App">
-      <h1 id="lol-header"><span className="hover-effect">L</span><span className="hover-effect">o</span><span className="hover-effect">L</span><span > </span><span className="hover-effect">R</span><span className="hover-effect">a</span><span className="hover-effect">n</span><span className="hover-effect">k</span ><span className="hover-effect">e</span><span className="hover-effect">d</span><span> </span><span className="hover-effect">R</span><span className="hover-effect">e</span><span className="hover-effect">p</span><span className="hover-effect">o</span><span className="hover-effect">r</span><span className="hover-effect">t</span></h1>
+      <h1 id="lol-header"><span className="hover-effect">L</span><span className="hover-effect">o</span><span className="hover-effect">L</span><span> </span><span className="hover-effect">R</span><span className="hover-effect">a</span><span className="hover-effect">n</span><span className="hover-effect">k</span ><span className="hover-effect">e</span><span className="hover-effect">d</span><span> </span><span className="hover-effect">R</span><span className="hover-effect">e</span><span className="hover-effect">p</span><span className="hover-effect">o</span><span className="hover-effect">r</span><span className="hover-effect">t</span></h1>
 
       <div id="bg-img-div">
-        <h2 id="leaderboards-header">Leaderboards (North America)</h2>
+        <h2 id="leaderboards-header">NA Leaderboards</h2>
         {/* <p>Select a tier to view current rankings</p> */}
         {/* <p>Sorted by League Points</p> */}
 
